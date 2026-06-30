df04ac85813a7b08eed815539c32bfd7a6d1c901
Aido Nayaka <aidonayaka4@gmail.com>
2026-06-30 12:46:53 +0700

    fix: change port

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
   console.log(`SEAPEDIA API running on http://localhost:${port}/api`);
 }
 bootstrap();
